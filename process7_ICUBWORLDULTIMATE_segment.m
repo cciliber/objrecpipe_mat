2853856c44bfd92f235bd7bd09808ad8272ac915
Giulia Pasquale <giulia.pasquale@iit.it>
2015-10-25 15:07:29 +0100

    added code to segment the dset

--- process7_ICUBWORLDULTIMATE_segment.m
+++ process7_ICUBWORLDULTIMATE_segment.m
@@ -9,8 +9,18 @@ day = 4;
 camera = 'right';
 
 in_root_path = fullfile('/media/giulia/DATA/ICUBWORLD_ULTIMATE_folderized_jpg', ['day' num2str(day)]);
+
+%%
+
 out_root_path_bb_disp = fullfile('/media/giulia/DATA/ICUBWORLD_ULTIMATE_folderized_jpg_BB_disp', ['day' num2str(day)]);
 out_root_path_centroid_disp = fullfile('/media/giulia/DATA/ICUBWORLD_ULTIMATE_folderized_jpg_BB_disp', ['day' num2str(day)]);
+
+box_radius = 127; % half side of the bounding box
+% e.g. 127 to crop 2x127+1+1 = 256 squared images
+% e.g. 63 to crop 2x63+1+1 = 128 squared images
+% e.g. 31 to crop 2x31+1+1 = 64 squared images
+
+%%
 
 feat = Features.GenericFeature();
 feat.assign_registry_and_tree_from_folder(in_root_path, [], [], [], []);
@@ -44,17 +54,41 @@ for ii=1:length(feat.Registry)
     
     I = imread(fullfile(in_root_path, feat.Registry{ii}));
     
-    xmin = img_info{1}(img_counter);
-    ymin = img_info{1}(img_counter);
-    width = img_info{1}(img_counter);
-    height = img_info{1}(img_counter);
+    xc = img_info{4}(img_counter);
+    yc = img_info{5}(img_counter);
     
-    xc = ;
-    yc = ;
+    radius = min(box_radius,xc-1);
+    radius = min(radius,yc-1);
+    radius = min(radius,size(I,2)-xc);
+    radius = min(radius,size(I,1)-yc);
+    
+    xmin = img_info{7}(img_counter);
+    ymin = img_info{8}(img_counter);
+    width = img_info{9}(img_counter);
+    height = img_info{10}(img_counter);
+      
+    if radius>10
+        
+        radius2 = radius*2+1;
+        
+        xminc = xc - radius;
+        yminc = yc - radius;
+        widthc = radius2;
+        heightc = radius2;
+    
+    else
+        
+        disp(['SKIPPED: ' feat.Registry{ii} '.']);
+        
+    end
     
     I2 = imcrop(I, [xmin ymin width height]);
+    I3 = imcrop(I, [xminc yminc widthc heightc]);
     
+    imshow(I2);
     imwrite(I2, fullfile(out_root_path_bb_disp, feat.Registry{ii}));
+    imshow(I3);
+    imwrite(I3, fullfile(out_root_path_centroid_disp, feat.Registry{ii}));
     
     img_counter = img_counter + 1;
     
