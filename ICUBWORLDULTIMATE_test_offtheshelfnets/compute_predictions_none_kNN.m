a9dcd08feec8c21281d3ccf1ffc3d0f8a4c148f0
Giulia Pasquale <giulia.pasquale@iit.it>
2015-11-09 11:23:09 +0100

    corrected labels of cat not in imnet and corrected knn code

--- ICUBWORLDULTIMATE_test_offtheshelfnets/compute_predictions_none_kNN.m
+++ ICUBWORLDULTIMATE_test_offtheshelfnets/compute_predictions_none_kNN.m
@@ -1,10 +1,7 @@
 %% Setup 
 
-FEATURES_DIR = '/data/giulia/REPOS/objrecpipe_mat';
+FEATURES_DIR = '/Users/giulia/REPOS/objrecpipe_mat';
 addpath(genpath(FEATURES_DIR));
-
-gurls_setup('/data/REPOS/GURLS/');
-vl_feat_setup();
 
 %% Dataset info
 
@@ -34,12 +31,12 @@ model = 'googlenet';
 %model = 'bvlc_reference_caffenet';
 %model = 'vgg';
 
-%dset_dir = '/data/giulia/DATASETS/iCubWorldUltimate_centroid384_disp_finaltree';
-%dset_dir = '/data/giulia/DATASETS/iCubWorldUltimate_bb60_disp_finaltree';
-dset_dir = '/data/giulia/DATASETS/iCubWorldUltimate_centroid256_disp_finaltree';
-%dset_dir = '/data/giulia/DATASETS/iCubWorldUltimate_bb30_disp_finaltree';
+%dset_dir = '/Volumes/MyPassport/iCubWorldUltimate_centroid384_disp_finaltree';
+%dset_dir = '/Volumes/MyPassport/iCubWorldUltimate_bb60_disp_finaltree';
+dset_dir = '/Volumes/MyPassport/iCubWorldUltimate_centroid256_disp_finaltree';
+%dset_dir = '/Volumes/MyPassport/iCubWorldUltimate_bb30_disp_finaltree';
 
-reg_dir = '/data/giulia/DATASETS/iCubWorldUltimate_digit_registries/test_offtheshelfnets';
+reg_dir = '/Volumes/MyPassport/iCubWorldUltimate_digit_registries/test_offtheshelfnets';
 check_input_dir(reg_dir);
 
 input_dir = fullfile([dset_dir '_experiments'], 'test_offtheshelfnets', 'scores', model);
@@ -77,28 +74,28 @@ for sidx=1:length(set_names)
         loader = Features.GenericFeature();
         loader.assign_registry_and_tree_from_file(reg_path, [], []);
         
-        fdirs = regexp(loader.Registry, '/', 'split');
+        flist_splitted = regexp(loader.Registry, '/', 'split');
         clear loader;
-        fdirs = vertcat(fdirs{:});
-        fdirs(:,1) = [];
+        flist_splitted = vertcat(flist_splitted{:});
+        flist_splitted(:,1) = [];
         
-        tobeloaded = zeros(length(fdirs), 1);
+        tobeloaded = zeros(length(flist_splitted), 1);
         
         for oo=obj_list
             
-            oo_tobeloaded = strcmp(fdirs(:,1), strcat(cat_names{cc}, num2str(oo)));
+            oo_tobeloaded = strcmp(flist_splitted(:,1), strcat(cat_names{cc}, num2str(oo)));
             
             for tt=transf_list
                 
-                tt_tobeloaded = oo_tobeloaded & strcmp(fdirs(:,2), transf_names(tt));
+                tt_tobeloaded = oo_tobeloaded & strcmp(flist_splitted(:,2), transf_names(tt));
                 
                 for dd=day_list
                     
-                    dd_tobeloaded = tt_tobeloaded & strcmp(fdirs(:,3), day_names(dd));
+                    dd_tobeloaded = tt_tobeloaded & strcmp(flist_splitted(:,3), day_names(dd));
                     
                     for ee=camera_list
                         
-                        ee_tobeloaded = dd_tobeloaded & strcmp(fdirs(:,4), camera_names(ee));
+                        ee_tobeloaded = dd_tobeloaded & strcmp(flist_splitted(:,4), camera_names(ee));
                         
                         tobeloaded = tobeloaded + ee_tobeloaded;
                         
@@ -107,7 +104,7 @@ for sidx=1:length(set_names)
             end
         end
         
-        REG{opts.Cat(cat_names{cc})} = fullfile(fdirs(tobeloaded==1,1), fdirs(tobeloaded==1,2), fdirs(tobeloaded==1,3), fdirs(tobeloaded==1,4), fdirs(tobeloaded==1,5));
+        REG{opts.Cat(cat_names{cc})} = fullfile(flist_splitted(tobeloaded==1,1), flist_splitted(tobeloaded==1,2), flist_splitted(tobeloaded==1,3), flist_splitted(tobeloaded==1,4), flist_splitted(tobeloaded==1,5));
         Y{opts.Cat(cat_names{cc})} = ones(length(REG{opts.Cat(cat_names{cc})}), 1)*double(opts.Cat_ImnetLabels(cat_names{cc}));
         X{opts.Cat(cat_names{cc})} = zeros(length(REG{opts.Cat(cat_names{cc})}), 1000);
         for ff=1:length(REG{opts.Cat(cat_names{cc})})
@@ -135,9 +132,9 @@ for sidx=1:length(set_names)
         Ypred{opts.Cat(cat_names{cc})} = zeros(N, 1);
         Nbatches = ceil(N/batch_size);
         
-        for idx=1:Nbatches
-            [~, I] = max(X{opts.Cat(cat_names{cc})}(((idx-1)*batch_size+1):min(idx*batch_size, N),:), [], 2);
-            Ypred{opts.Cat(cat_names{cc})}((((idx-1)*batch_size+1):min(idx*batch_size, N))) = I-1;
+        for bidx=1:Nbatches
+            [~, I] = max(X{opts.Cat(cat_names{cc})}(((bidx-1)*batch_size+1):min(bidx*batch_size, N),:), [], 2);
+            Ypred{opts.Cat(cat_names{cc})}((((bidx-1)*batch_size+1):min(bidx*batch_size, N))) = I-1;
         end
     end
     
@@ -147,44 +144,104 @@ end
 
 clear tobeloaded oo_tobeloaded tt_tobeloaded dd_tobeloaded ee_tobeloaded
 
-%% Compute kNN predictions on one set, keeping the other as training set
+%% Compute & save D matrix
 
-k = 1;
+XX = cell(length(set_names), 1);
+XX2 = cell(length(set_names), 1);
+YY = cell(length(set_names), 1);
 
-XX = cell(length(set_names),1);
-YY = cell(length(set_names),1);
+N = zeros(length(set_names), 1);
+NframesPerCat = cell(length(set_names), 1);
 
 for sidx=1:length(set_names)
     
     set_name = set_names{sidx};
-    load(fullfile(output_dir, ['X_' set_name '.mat']));
-    load(fullfile(output_dir, ['Y_' set_name '.mat']));
-    XX{sidx} = X;
-    YY{sidx} = Y;
     
+    m = matfile(fullfile(output_dir, ['X_' set_name(sidx) '.mat']));
+    NframesPerCat{sidx} = cellfun(m.X, @length);
+    XX{sidx} = cell2mat(m.X);
+    
+    m = matfile(fullfile(output_dir, ['Y_' set_name(sidx) '.mat']));
+    YY{sidx} = cell2mat(m.Y);
+    
+    XX2{sidx} = sum(XX{sidx}.*XX{sidx},2);
+    
+    N(sidx) = size(XX{sidx},1);
+        
 end
 
-for sidx=1:length(set_names)
-    
-    set_name = set_names{sidx};
-    
-    other_sidx = -sidx + 3;
-    
-    Ypred = cell(Ncat, 1);
-    batch_size = 10000;
-    
-    for cc=cat_idx
-    
-        N = size(XX{sidx}{opts.Cat(cat_names{cc})},1);
-        Ypred{opts.Cat(cat_names{cc})} = zeros(N, 1);
-        Nbatches = ceil(N/batch_size);
+batch_size = 10000;
+m = matfile(fullfile(output_dir, 'D.mat'), 'Writable', true);
 
-        for idx=1:Nbatches 
-            Ypred{opts.Cat(cat_names{cc})}((((idx-1)*batch_size+1):min(idx*batch_size, N))) = kNNClassify_multiclass(cell2mat(XX{other_sidx}), cell2mat(YY{other_sidx}), k, XX{sidx}{opts.Cat(cat_names{cc})}(((idx-1)*batch_size+1):min(idx*batch_size, N),:));
-            disp([set_name ': ' num2str(cc) ' ' num2str(idx)]);
-        end
+Nbatches = ceil(N(2)/batch_size);
+
+for bidx=1:Nbatches
+    
+    start_idx = (bidx-1)*batch_size+1;
+    end_idx = min(bidx*batch_size, N(2));
+    n = end_idx-start_idx+1;
+    
+    m.D(:, start_idx:end_idx) = XX2{1}*ones(1,n) - 2 * XX{1} * XX{2}(start_idx:end_idx, :)' + ones(N(1),1)*(XX2{2}(start_idx:end_idx))';
+            
+end
+
+%% Compute kNN predictions
+
+K = 1;
+
+m = matfile(fullfile(output_dir, 'D.mat'));
+
+% On set 2
+
+Nbatches = ceil(N(2)/batch_size);
+
+Ypred = zeros(N(2), 1);
+k = min(K, N(1));
+
+for bidx=1:Nbatches
+    
+    start_idx = (bidx-1)*batch_size+1;
+    end_idx = min(bidx*batch_size, N(2));
+    
+    [~, I] = sort(m.D(:, start_idx:end_idx), 1);
+    idx = I(1:k, :);
+    
+    if k==1
+        Ypred(start_idx:end_idx) = mode(YY{1}(idx)',1)';
+    else
+        Ypred(start_idx:end_idx) = mode(YY{1}(idx),1)';
+    end
+           
+end
+
+Ypred = mat2cell(Ypred, NframesPerCat{2});
+set_name = set_names{2};
+save(fullfile(output_dir, ['Y_' num2str(k) 'NN_' set_name '.mat']), 'Ypred', '-v7.3');
+
+% On set 1
+
+Nbatches = ceil(N(1)/batch_size);
+
+Ypred = zeros(N(1), 1);
+k = min(K, N(2));
+
+for bidx=1:Nbatches
+    
+    start_idx = (bidx-1)*batch_size+1;
+    end_idx = min(bidx*batch_size, N(1));
+      
+    [~, I] = sort(m.D(start_idx:end_idx,:),2);
+    idx = I(:, 1:k)';
+    
+    if k==1
+        Ypred(start_idx:end_idx) = mode(YY{2}(idx)',1)';
+    else
+        Ypred(start_idx:end_idx) = mode(YY{2}(idx),1)';
     end
     
-    save(fullfile(output_dir, ['Y_kNN_' set_name '.mat']), 'Ypred', '-v7.3');
-    
 end
+
+Ypred = mat2cell(Ypred, NframesPerCat{1});
+set_name = set_names{2};
+save(fullfile(output_dir, ['Y_kNN_' set_name '.mat']), 'Ypred', '-v7.3');
+    
